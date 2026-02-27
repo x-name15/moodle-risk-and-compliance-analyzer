9e7d9aad5942117b4edca31c0d9f769ed5ea7cc9
Felix Manrique | Flez | Mr Jacket <69986110+x-name15@users.noreply.github.com>
2026-02-27 11:43:05 +0100

    Update plugin version to 2026022700

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_mrca';
-$plugin->version = 2026022600;
+$plugin->version = 2026022700;
 $plugin->requires = 2022112800;
 $plugin->supported = [401, 501];
 $plugin->maturity = MATURITY_STABLE;
